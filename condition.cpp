93e818f5c6661b5f36642825a57b6f1f4b8ebec2
Sadi-Rahman <sadiakash1309@gmail.com>
2022-01-27 17:39:16 +0600

    Condition

---
+++ condition.cpp
@@ -0,0 +1,44 @@
+#include <cmath>
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1;
+    
+    cin>>num1;
+    
+    
+    if(num1==1) {
+        cout<<"Your Number is one - "<<num1;
+    }
+    else if(num1==2) {
+        cout<<"Your Number is two - "<<num1;
+    }
+    else if(num1==3) {
+        cout<<"Your Number is three - "<<num1;;
+    }
+    else if(num1==4) {
+        cout<<"Your Number is four - "<<num1;
+    }
+    else if(num1==5) {
+        cout<<"Your Number is five - "<<num1;;
+    }else if(num1==6) {
+        cout<<"Your Number is six - "<<num1;
+    }
+    else if(num1==7) {
+        cout<<"Your Number is seven - "<<num1;;
+    }else if(num1==8) {
+        cout<<"Your Number is eight - "<<num1;
+    }
+    else if(num1==9) {
+        cout<<"Your Number is nine - "<<num1;;
+    }
+    else {
+        cout<<"Your Number is greater than nine "<<num1;
+    }
+
+    return 0;
+}
